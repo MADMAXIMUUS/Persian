1c1f5eaa7592eecc3bcfbdf0869223b7a2cc1e9d
MADMAXIMUUS <maxim00nechaev@gmail.com>
2023-12-14 18:56:07 +0300

    Add TimePickerDialog

---
+++ persian/src/main/java/io/github/madmaximuus/persian/timePickerDialog/TimePickerUtils.kt
@@ -0,0 +1,22 @@
+package io.github.madmaximuus.persian.timePickerDialog
+
+import java.time.LocalTime
+
+internal fun parseTime(time: LocalTime, is24TimeFormat: Boolean): PickerTime {
+    return PickerTime(
+        hours = if (is24TimeFormat.not() && time.hour > 12) time.hour - 12 else time.hour,
+        minutes = time.minute,
+        if (is24TimeFormat) null else if (time.hour > 12) TimePickerDialogAmPM.PM else TimePickerDialogAmPM.AM
+    )
+}
+
+internal fun PickerTime.toLocalTime(): LocalTime {
+    return LocalTime.of(
+        when (timesOfDay) {
+            TimePickerDialogAmPM.AM -> hours % 12
+            TimePickerDialogAmPM.PM  -> hours % 12 + 12
+            else -> hours
+        },
+        minutes
+    )
+}
