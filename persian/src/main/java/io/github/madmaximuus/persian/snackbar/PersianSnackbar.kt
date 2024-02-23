03cecf2dd1130312f08266ee4513f485c8eb117a
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-02-23 23:25:55 +0300

    Update Snackbar

--- persian/src/main/java/io/github/madmaximuus/persian/snackbar/PersianSnackbar.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/snackbar/PersianSnackbar.kt
@@ -32,109 +32,106 @@ data class PersianSnackbarVisuals(
     val onDismiss: (() -> Unit)? = null
 ) : SnackbarVisuals
 
-object PersianSnackbar {
+@Composable
+fun PersianSnackbar(
+    text: String,
+    left: PersianSnackbarLeft? = null,
+    colors: SnackbarColors = PersianSnackbarDefaults.colors(),
+    sizes: SnackbarSizes = PersianSnackbarDefaults.sizes(),
+    right: PersianSnackbarRight? = null,
+    showOnTop: Boolean = false
+) {
+    val configuration = LocalConfiguration.current
 
-    @Composable
-    fun Primary(
-        text: String,
-        left: PersianSnackbarLeft? = null,
-        colors: SnackbarColors = PersianSnackbarDefaults.colors(),
-        sizes: SnackbarSizes = PersianSnackbarDefaults.sizes(),
-        right: PersianSnackbarRight? = null,
-        showOnTop: Boolean = false
-    ) {
-        val configuration = LocalConfiguration.current
+    val spacingLarge = MaterialTheme.spacing.large
 
-        val spacingLarge = MaterialTheme.spacing.large
-
-        val padding by remember {
-            mutableStateOf(
-                if (showOnTop) {
-                    PaddingValues(
-                        start = spacingLarge,
-                        top = 50.dp,
-                        bottom = configuration.screenHeightDp.dp - 100.dp,
-                        end = spacingLarge
-                    )
-                } else {
-                    PaddingValues(horizontal = spacingLarge, vertical = 70.dp)
-                }
-            )
-        }
-
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(min = 52.dp)
-                .padding(padding),
-            shape = sizes.cornerRadius,
-            color = colors.backgroundColor,
-            content = {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .heightIn(min = 52.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    when (left) {
-                        is PersianSnackbarLeft.Avatar -> {
-                            PersianSnackbarLeftAvatar(avatarUrl = left.avatarUrl)
-                        }
-
-                        is PersianSnackbarLeft.Icon -> {
-                            PersianSnackbarLeftIcon(
-                                icon = left.icon,
-                                color = left.color,
-                                contentDescription = ""
-                            )
-                        }
-
-                        is PersianSnackbarLeft.Image -> {
-                            PersianSnackbarLeftImage(imageUrl = left.imageUrl)
-                        }
-
-                        is PersianSnackbarLeft.Progress -> {
-                            PersianSnackbarLeftProgress(progress = left.progress)
-                        }
-
-                        null -> {}
-                    }
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(
-                                    horizontal = MaterialTheme.spacing.medium,
-                                    vertical = MaterialTheme.spacing.medium
-                                ),
-                            text = text,
-                            style = sizes.textStyle,
-                            color = colors.textColor
-                        )
-                    }
-                    when (right) {
-                        is PersianSnackbarRight.Action -> {
-                            PersianSnackbarRightAction(
-                                text = right.text,
-                                onClick = right.onClick
-                            )
-                        }
-
-                        is PersianSnackbarRight.Close -> {
-                            PersianSnackbarRightClose(
-                                onClick = right.onClick
-                            )
-                        }
-
-                        null -> {}
-                    }
-                }
+    val padding by remember {
+        mutableStateOf(
+            if (showOnTop) {
+                PaddingValues(
+                    start = spacingLarge,
+                    top = 50.dp,
+                    bottom = configuration.screenHeightDp.dp - 100.dp,
+                    end = spacingLarge
+                )
+            } else {
+                PaddingValues(horizontal = spacingLarge, vertical = 70.dp)
             }
         )
     }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 52.dp)
+            .padding(padding),
+        shape = sizes.cornerRadius,
+        color = colors.backgroundColor,
+        content = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(min = 52.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                when (left) {
+                    is PersianSnackbarLeft.Avatar -> {
+                        PersianSnackbarLeftAvatar(avatarUrl = left.avatarUrl)
+                    }
+
+                    is PersianSnackbarLeft.Icon -> {
+                        PersianSnackbarLeftIcon(
+                            icon = left.icon,
+                            color = left.color,
+                            contentDescription = ""
+                        )
+                    }
+
+                    is PersianSnackbarLeft.Image -> {
+                        PersianSnackbarLeftImage(imageUrl = left.imageUrl)
+                    }
+
+                    is PersianSnackbarLeft.Progress -> {
+                        PersianSnackbarLeftProgress(progress = left.progress)
+                    }
+
+                    null -> {}
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                horizontal = MaterialTheme.spacing.medium,
+                                vertical = MaterialTheme.spacing.medium
+                            ),
+                        text = text,
+                        style = sizes.textStyle,
+                        color = colors.textColor
+                    )
+                }
+                when (right) {
+                    is PersianSnackbarRight.Action -> {
+                        PersianSnackbarRightAction(
+                            text = right.text,
+                            onClick = right.onClick
+                        )
+                    }
+
+                    is PersianSnackbarRight.Close -> {
+                        PersianSnackbarRightClose(
+                            onClick = right.onClick
+                        )
+                    }
+
+                    null -> {}
+                }
+            }
+        }
+    )
 }
