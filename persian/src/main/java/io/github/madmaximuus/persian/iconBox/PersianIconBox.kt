f994eb7c6f7dd36b4965ae7295cb99040a25f78c
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-04-05 00:55:18 +0300

    Update IconBox

--- persian/src/main/java/io/github/madmaximuus/persian/iconBox/PersianIconBox.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/iconBox/PersianIconBox.kt
@@ -2,6 +2,7 @@ package io.github.madmaximuus.persian.iconBox
 
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -11,17 +12,13 @@ fun PersianIconBox(
     modifier: Modifier = Modifier,
     icon: Painter,
     size: IconBoxSize = PersianIconBoxDefaults.size24(),
-    colors: IconBoxColors = PersianIconBoxDefaults.colors(),
-    enabled: Boolean = true,
-    isError: Boolean = false,
     contentDescription: String = ""
 ) {
-    val tint = colors.iconColor(enabled = enabled, isError = isError).value
     Icon(
         modifier = modifier
             .size(size.width),
         painter = icon,
         contentDescription = contentDescription,
-        tint = tint
+        tint = LocalContentColor.current
     )
 }
--- persian/src/main/java/io/github/madmaximuus/persian/iconBox/PersianIconBoxDefaults.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/iconBox/PersianIconBoxDefaults.kt
@@ -1,22 +1,22 @@
 package io.github.madmaximuus.persian.iconBox
 
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import io.github.madmaximuus.persian.foundation.PersianContentStateDisabled
-import io.github.madmaximuus.persian.foundation.extendedColorScheme
 
 object PersianIconBoxDefaults {
     @Composable
     fun size48(
         width: Dp = 48.dp,
+        padding: Dp = 0.dp
+    ) = remember(width, padding) {
+        IconBoxSize(width, padding)
+    }
+
+    @Composable
+    fun size40(
+        width: Dp = 40.dp,
         padding: Dp = 0.dp
     ) = remember(width, padding) {
         IconBoxSize(width, padding)
@@ -63,48 +63,19 @@ object PersianIconBoxDefaults {
     }
 
     @Composable
-    fun colors(
-        errorColor: Color = MaterialTheme.extendedColorScheme.error,
-        disabledColor: Color = MaterialTheme.extendedColorScheme.onSurface.copy(
-            alpha = PersianContentStateDisabled
-        )
-    ): IconBoxColors =
-        IconBoxColors(
-            errorColor = errorColor,
-            disabledColor = disabledColor
-        )
-}
+    fun size16(
+        width: Dp = 16.dp,
+        padding: Dp = 0.dp
+    ) = remember(width, padding) {
+        IconBoxSize(width, padding)
+    }
 
-@Immutable
-class IconBoxColors internal constructor(
-    private val disabledColor: Color,
-    private val errorColor: Color
-) {
     @Composable
-    internal fun iconColor(
-        enabled: Boolean,
-        isError: Boolean
-    ): State<Color> {
-        val targetValue = when {
-            !enabled -> disabledColor
-            isError -> errorColor
-            else -> LocalContentColor.current
-        }
-        return rememberUpdatedState(targetValue)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || other !is IconBoxColors) return false
-
-        if (errorColor != other.errorColor) return false
-        return disabledColor == other.disabledColor
-    }
-
-    override fun hashCode(): Int {
-        var result = errorColor.hashCode()
-        result = 31 * result + disabledColor.hashCode()
-        return result
+    fun size12(
+        width: Dp = 12.dp,
+        padding: Dp = 0.dp
+    ) = remember(width, padding) {
+        IconBoxSize(width, padding)
     }
 }
 
