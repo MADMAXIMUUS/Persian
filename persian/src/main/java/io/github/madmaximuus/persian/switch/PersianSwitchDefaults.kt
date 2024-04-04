51f53442084472377d9de620d147337321391e39
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-04-05 00:58:40 +0300

    Update Switch

--- persian/src/main/java/io/github/madmaximuus/persian/switch/PersianSwitchDefaults.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/switch/PersianSwitchDefaults.kt
@@ -4,11 +4,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import io.github.madmaximuus.persian.foundation.PersianContentStateDisabled
-import io.github.madmaximuus.persian.foundation.PersianStatesDisabled
-import io.github.madmaximuus.persian.foundation.elevation
 import io.github.madmaximuus.persian.foundation.extendedColorScheme
-import io.github.madmaximuus.persian.foundation.surfaceColorAtElevation
+import io.github.madmaximuus.persian.foundation.state12
+import io.github.madmaximuus.persian.foundation.state38
 
 object PersianSwitchDefaults {
 
@@ -19,30 +17,17 @@ object PersianSwitchDefaults {
         checkedBorderColor: Color = MaterialTheme.extendedColorScheme.primary,
         checkedIconColor: Color = MaterialTheme.extendedColorScheme.onPrimaryContainer,
         uncheckedThumbColor: Color = MaterialTheme.extendedColorScheme.onPrimaryContainer,
-        uncheckedTrackColor: Color = MaterialTheme.extendedColorScheme
-            .surfaceColorAtElevation(MaterialTheme.elevation.large),
+        uncheckedTrackColor: Color = MaterialTheme.extendedColorScheme.surface4,
         uncheckedBorderColor: Color = MaterialTheme.extendedColorScheme.primary,
         uncheckedIconColor: Color = MaterialTheme.extendedColorScheme.onPrimary,
         disabledCheckedThumbColor: Color = MaterialTheme.extendedColorScheme.surface,
-        disabledCheckedTrackColor: Color = MaterialTheme.extendedColorScheme.onSurface.copy(
-            alpha = PersianStatesDisabled
-        ),
-        disabledCheckedBorderColor: Color = MaterialTheme.extendedColorScheme.onSurface.copy(
-            alpha = PersianStatesDisabled
-        ),
-        disabledCheckedIconColor: Color = MaterialTheme.extendedColorScheme.onPrimaryContainer.copy(
-            alpha = PersianContentStateDisabled
-        ),
+        disabledCheckedTrackColor: Color = MaterialTheme.extendedColorScheme.onSurface.state12,
+        disabledCheckedBorderColor: Color = MaterialTheme.extendedColorScheme.onSurface.state12,
+        disabledCheckedIconColor: Color = MaterialTheme.extendedColorScheme.onPrimaryContainer.state38,
         disabledUncheckedThumbColor: Color = MaterialTheme.extendedColorScheme.surface,
-        disabledUncheckedTrackColor: Color = MaterialTheme.extendedColorScheme.onSurface.copy(
-            alpha = PersianStatesDisabled
-        ),
-        disabledUncheckedBorderColor: Color = MaterialTheme.extendedColorScheme.onSurface.copy(
-            alpha = PersianStatesDisabled
-        ),
-        disabledUncheckedIconColor: Color = MaterialTheme.extendedColorScheme.onSurface.copy(
-            alpha = PersianContentStateDisabled
-        ),
+        disabledUncheckedTrackColor: Color = MaterialTheme.extendedColorScheme.onSurface.state12,
+        disabledUncheckedBorderColor: Color = MaterialTheme.extendedColorScheme.onSurface.state12,
+        disabledUncheckedIconColor: Color = MaterialTheme.extendedColorScheme.onSurface.state38,
     ) = SwitchDefaults.colors(
         checkedThumbColor = checkedThumbColor,
         checkedTrackColor = checkedTrackColor,
@@ -61,6 +46,4 @@ object PersianSwitchDefaults {
         disabledUncheckedThumbColor = disabledUncheckedThumbColor,
         disabledUncheckedTrackColor = disabledUncheckedTrackColor
     )
-
-
 }
