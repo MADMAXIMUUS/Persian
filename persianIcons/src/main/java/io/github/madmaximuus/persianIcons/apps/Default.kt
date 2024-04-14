2c3710120d9ce03801ea5d3518109e37cc638183
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-04-14 23:41:13 +0300

    Add Apps icon

---
+++ persianIcons/src/main/java/io/github/madmaximuus/persianIcons/apps/Default.kt
@@ -0,0 +1,210 @@
+package io.github.madmaximuus.persianIcons.apps
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun appsDefault(): ImageVector {
+    val color = LocalContentColor.current
+    return remember {
+        ImageVector.Builder(
+            name = "apps-default",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(6f, 7.99994f)
+                curveTo(7.1046f, 7.9999f, 8f, 7.1045f, 8f, 5.9999f)
+                curveTo(8f, 4.8954f, 7.1046f, 3.9999f, 6f, 3.9999f)
+                curveTo(4.8954f, 3.9999f, 4f, 4.8954f, 4f, 5.9999f)
+                curveTo(4f, 7.1045f, 4.8954f, 7.9999f, 6f, 7.9999f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(8f, 12f)
+                curveTo(8f, 13.1046f, 7.1046f, 14f, 6f, 14f)
+                curveTo(4.8954f, 14f, 4f, 13.1046f, 4f, 12f)
+                curveTo(4f, 10.8954f, 4.8954f, 10f, 6f, 10f)
+                curveTo(7.1046f, 10f, 8f, 10.8954f, 8f, 12f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(8f, 18f)
+                curveTo(8f, 19.1046f, 7.1046f, 20f, 6f, 20f)
+                curveTo(4.8954f, 20f, 4f, 19.1046f, 4f, 18f)
+                curveTo(4f, 16.8954f, 4.8954f, 16f, 6f, 16f)
+                curveTo(7.1046f, 16f, 8f, 16.8954f, 8f, 18f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(14f, 18f)
+                curveTo(14f, 19.1046f, 13.1046f, 20f, 12f, 20f)
+                curveTo(10.8954f, 20f, 10f, 19.1046f, 10f, 18f)
+                curveTo(10f, 16.8954f, 10.8954f, 16f, 12f, 16f)
+                curveTo(13.1046f, 16f, 14f, 16.8954f, 14f, 18f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(17.9999f, 20f)
+                curveTo(19.1044f, 20f, 19.9999f, 19.1046f, 19.9999f, 18f)
+                curveTo(19.9999f, 16.8954f, 19.1044f, 16f, 17.9999f, 16f)
+                curveTo(16.8953f, 16f, 15.9999f, 16.8954f, 15.9999f, 18f)
+                curveTo(15.9999f, 19.1046f, 16.8953f, 20f, 17.9999f, 20f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(12f, 14f)
+                curveTo(13.1046f, 14f, 14f, 13.1046f, 14f, 12f)
+                curveTo(14f, 10.8954f, 13.1046f, 10f, 12f, 10f)
+                curveTo(10.8954f, 10f, 10f, 10.8954f, 10f, 12f)
+                curveTo(10f, 13.1046f, 10.8954f, 14f, 12f, 14f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(19.9999f, 12f)
+                curveTo(19.9999f, 13.1046f, 19.1044f, 14f, 17.9999f, 14f)
+                curveTo(16.8953f, 14f, 15.9999f, 13.1046f, 15.9999f, 12f)
+                curveTo(15.9999f, 10.8954f, 16.8953f, 10f, 17.9999f, 10f)
+                curveTo(19.1044f, 10f, 19.9999f, 10.8954f, 19.9999f, 12f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(14f, 5.99994f)
+                curveTo(14f, 7.1045f, 13.1046f, 7.9999f, 12f, 7.9999f)
+                curveTo(10.8954f, 7.9999f, 10f, 7.1045f, 10f, 5.9999f)
+                curveTo(10f, 4.8954f, 10.8954f, 3.9999f, 12f, 3.9999f)
+                curveTo(13.1046f, 3.9999f, 14f, 4.8954f, 14f, 5.9999f)
+                close()
+            }
+            path(
+                fill = SolidColor(color),
+                fillAlpha = 1.0f,
+                stroke = null,
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 1.0f,
+                strokeLineCap = StrokeCap.Butt,
+                strokeLineJoin = StrokeJoin.Miter,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(17.9999f, 7.99994f)
+                curveTo(19.1044f, 7.9999f, 19.9999f, 7.1045f, 19.9999f, 5.9999f)
+                curveTo(19.9999f, 4.8954f, 19.1044f, 3.9999f, 17.9999f, 3.9999f)
+                curveTo(16.8953f, 3.9999f, 15.9999f, 4.8954f, 15.9999f, 5.9999f)
+                curveTo(15.9999f, 7.1045f, 16.8953f, 7.9999f, 17.9999f, 7.9999f)
+                close()
+            }
+        }.build()
+    }
+}
+
+@Preview
+@Composable
+private fun IconPreview() {
+    MaterialTheme {
+        Surface {
+            Icon(
+                modifier = Modifier.size(100.dp),
+                imageVector = appsDefault(),
+                contentDescription = ""
+            )
+        }
+    }
+}
