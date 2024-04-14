0053dc8b8e340395f3c8333f4a25d575b0181a60
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-04-14 22:01:44 +0300

    Add Arrow Forward icon

---
+++ persianIcons/src/main/java/io/github/madmaximuus/persianIcons/arrowForward/Default.kt
@@ -0,0 +1,65 @@
+package io.github.madmaximuus.persianIcons.arrowForward
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun arrowForwardDefault(): ImageVector {
+    val color = LocalContentColor.current
+    return remember {
+        ImageVector.Builder(
+            name = "arrow-forward-default",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(color),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(19f, 12.4222f)
+                lineTo(13f, 7.42224f)
+                moveTo(19f, 12.4222f)
+                lineTo(13f, 17.4222f)
+                moveTo(19f, 12.4222f)
+                lineTo(5f, 12.4222f)
+            }
+        }.build()
+    }
+}
+
+@Preview
+@Composable
+private fun IconPreview() {
+    MaterialTheme {
+        Surface {
+            Icon(
+                modifier = Modifier.size(100.dp),
+                imageVector = arrowForwardDefault(),
+                contentDescription = ""
+            )
+        }
+    }
+}
