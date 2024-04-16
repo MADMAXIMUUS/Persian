890ca798ef02f287dc819839ffccdccfe0d37781
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-04-16 22:48:53 +0300

    Add Arrow Downward icon

---
+++ persianIcons/src/main/java/io/github/madmaximuus/persianIcons/arrowDownward/Default.kt
@@ -0,0 +1,65 @@
+package io.github.madmaximuus.persianIcons.arrowDownward
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun arrowDownwardDefault(): ImageVector {
+    val color = LocalContentColor.current
+    return remember {
+        ImageVector.Builder(
+            name = "arrow-downward-default",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(color),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(12f, 19f)
+                lineTo(17f, 13f)
+                moveTo(12f, 19f)
+                lineTo(7f, 13f)
+                moveTo(12f, 19f)
+                lineTo(12f, 5f)
+            }
+        }.build()
+    }
+}
+
+@Preview
+@Composable
+private fun IconPreview() {
+    MaterialTheme {
+        Surface {
+            Icon(
+                modifier = Modifier.size(100.dp),
+                imageVector = arrowDownwardDefault(),
+                contentDescription = ""
+            )
+        }
+    }
+}
