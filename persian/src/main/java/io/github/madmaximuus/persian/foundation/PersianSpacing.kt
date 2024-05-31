67c4d075c62308a63c41c0291fa58fc65ada731a
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-05-31 20:02:55 +0300

    Update spacing

--- persian/src/main/java/io/github/madmaximuus/persian/foundation/PersianSpacing.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/foundation/PersianSpacing.kt
@@ -10,13 +10,18 @@ import androidx.compose.ui.unit.dp
 
 @Immutable
 data class PersianSpacing(
-    val extraExtraSmall: Dp = 2.dp,
-    val extraSmall: Dp = 4.dp,
-    val small: Dp = 8.dp,
-    val medium: Dp = 12.dp,
-    val large: Dp = 16.dp,
-    val extraLarge: Dp = 20.dp,
-    val extraExtraLarge: Dp = 24.dp
+    val size2: Dp = 2.dp,
+    val size4: Dp = 4.dp,
+    val size6: Dp = 6.dp,
+    val size8: Dp = 8.dp,
+    val size10: Dp = 10.dp,
+    val size12: Dp = 12.dp,
+    val size14: Dp = 14.dp,
+    val size16: Dp = 16.dp,
+    val size18: Dp = 18.dp,
+    val size20: Dp = 20.dp,
+    val size22: Dp = 22.dp,
+    val size24: Dp = 24.dp
 )
 
 val LocalPersianSpacing = staticCompositionLocalOf { PersianSpacing() }
