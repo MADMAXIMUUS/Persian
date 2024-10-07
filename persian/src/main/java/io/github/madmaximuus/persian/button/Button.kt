b4ab3596b9302dcf92e9f05e0777c8430484b6cc
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-10-08 00:49:26 +0300

    Documentation: Button

--- persian/src/main/java/io/github/madmaximuus/persian/button/Button.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/button/Button.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
