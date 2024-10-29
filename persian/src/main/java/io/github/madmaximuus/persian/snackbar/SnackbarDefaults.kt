b19d76993ef2f0b165d119aee8d45f9e99a93fbe
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-10-30 00:04:42 +0300

    Documentation: Snackbar

--- persian/src/main/java/io/github/madmaximuus/persian/snackbar/SnackbarDefaults.kt
+++ persian/src/main/java/io/github/madmaximuus/persian/snackbar/SnackbarDefaults.kt
@@ -22,8 +22,27 @@ import io.github.madmaximuus.persian.progressIndicator.CircularProgressBarSizes
 import io.github.madmaximuus.persian.progressIndicator.ProgressBarColors
 import io.github.madmaximuus.persian.progressIndicator.ProgressIndicatorDefaults
 
+/**
+ * Contains all default values used by snackbar
+ */
 object SnackbarDefaults {
 
+    /**
+     * A composable function to create a [SnackbarColors] instance with customizable color properties.
+     *
+     * This function allows you to specify the colors for various elements of the Snackbar, including
+     * the container, message text, left visual elements (icon, avatar, timer, image), and right visual
+     * elements (close button, action button).
+     *
+     * @param containerColor The background color of the Snackbar container. Defaults to
+     * @param messageColor The color of the message text displayed in the Snackbar. Defaults to
+     * @param leftIconColor The color of the icon displayed on the left side of the Snackbar. Defaults to
+     * @param leftAvatarColors The color scheme for the avatar displayed on the left side of the Snackbar.
+     * @param leftTimerColors The color scheme for the timer progress bar displayed on the left side of the
+     * @param leftImageColors The color scheme for the image displayed on the left side of the Snackbar.
+     * @param rightCloseColor The color of the close button displayed on the right side of the Snackbar.
+     * @param rightActionColor The color of the action button displayed on the right side of the Snackbar.
+     */
     @Composable
     fun colors(
         containerColor: Color = PersianTheme.colorScheme.surfaceContainerHighest,
@@ -49,6 +68,22 @@ object SnackbarDefaults {
             rightActionColor = rightActionColor
         )
 
+    /**
+     * A composable function to create a [SnackbarSizes] instance with customizable size and style properties.
+     *
+     * This function allows you to specify the sizes and styles for various elements of the Snackbar, including
+     * the container shape, message text style, left visual elements (icon, avatar, timer, image), and right
+     * visual elements (close button, action button).
+     *
+     * @param containerShape The shape of the Snackbar container.
+     * @param messageTextStyle The text style for the message displayed in the Snackbar.
+     * @param leftIconSizes The size scheme for the icon displayed on the left side of the Snackbar.
+     * @param leftAvatarSizes The size scheme for the avatar displayed on the left side of the Snackbar.
+     * @param leftTimerSizes The size scheme for the timer progress bar displayed on the left side of the Snackbar.
+     * @param leftImageSizes The size scheme for the image displayed on the left side of the Snackbar.
+     * @param rightCloseSizes The size scheme for the close button displayed on the right side of the Snackbar.
+     * @param rightActionSizes The size scheme for the action button displayed on the right side of the Snackbar.
+     */
     @Composable
     fun sizes(
         containerShape: Shape = PersianTheme.shapes.shape16,
@@ -75,6 +110,22 @@ object SnackbarDefaults {
     )
 }
 
+/**
+ * An immutable class representing the color scheme for a Snackbar.
+ *
+ * This class encapsulates various color properties used in different parts of the Snackbar,
+ * including the container, message, left visual elements (icon, avatar, timer, image),
+ * and right visual elements (close button, action button).
+ *
+ * @property containerColor The background color of the Snackbar container.
+ * @property messageColor The color of the message text displayed in the Snackbar.
+ * @property leftIconColor The color of the icon displayed on the left side of the Snackbar.
+ * @property leftAvatarColors The color scheme for the avatar displayed on the left side of the Snackbar.
+ * @property leftTimerColors The color scheme for the timer progress bar displayed on the left side of the Snackbar.
+ * @property leftImageColors The color scheme for the image displayed on the left side of the Snackbar.
+ * @property rightCloseColor The color of the close button displayed on the right side of the Snackbar.
+ * @property rightActionColor The color of the action button displayed on the right side of the Snackbar.
+ */
 @Immutable
 class SnackbarColors internal constructor(
     internal val containerColor: Color,
@@ -145,6 +196,22 @@ class SnackbarColors internal constructor(
     }
 }
 
+/**
+ * An immutable class representing the size and style scheme for a Snackbar.
+ *
+ * This class encapsulates various size and style properties used in different parts of the Snackbar,
+ * including the container shape, message text style, left visual elements (icon, avatar, timer, image),
+ * and right visual elements (close button, action button).
+ *
+ * @property containerShape The shape of the Snackbar container.
+ * @property messageTextStyle The text style for the message displayed in the Snackbar.
+ * @property leftIconSizes The size scheme for the icon displayed on the left side of the Snackbar.
+ * @property leftAvatarSizes The size scheme for the avatar displayed on the left side of the Snackbar.
+ * @property leftTimerSizes The size scheme for the timer progress bar displayed on the left side of the Snackbar.
+ * @property leftImageSizes The size scheme for the image displayed on the left side of the Snackbar.
+ * @property rightCloseSizes The size scheme for the close button displayed on the right side of the Snackbar.
+ * @property rightActionSizes The size scheme for the action button displayed on the right side of the Snackbar.
+ */
 @Immutable
 class SnackbarSizes internal constructor(
     internal val containerShape: Shape,
