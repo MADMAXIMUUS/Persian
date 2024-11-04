e6e946cd8ea6afe6b781ee6352396474bacae67c
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-11-04 11:50:53 +0300

    Update: Charts

--- app-persian-showcase/src/main/java/ru/rabbit/persian/appShowcase/screens/Charts.kt
+++ app-persian-showcase/src/main/java/ru/rabbit/persian/appShowcase/screens/Charts.kt
@@ -1,21 +1,20 @@
 package ru.rabbit.persian.appShowcase.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import io.github.madmaximuus.persian.charts.donut.DonutChart
 import io.github.madmaximuus.persian.charts.donut.DonutChartDefaults
@@ -24,8 +23,12 @@ import io.github.madmaximuus.persian.charts.pie.PieChartDefaults
 import io.github.madmaximuus.persian.charts.util.ChartConfig
 import io.github.madmaximuus.persian.charts.util.ChartData
 import io.github.madmaximuus.persian.checkboxes.Checkbox
+import io.github.madmaximuus.persian.forms.Checkboxes
+import io.github.madmaximuus.persian.forms.Form
+import io.github.madmaximuus.persian.forms.RadioButton
+import io.github.madmaximuus.persian.forms.RadioButtons
+import io.github.madmaximuus.persian.forms.Subhead
 import io.github.madmaximuus.persian.foundation.PersianTheme
-import io.github.madmaximuus.persian.text.Text
 import io.github.madmaximuus.persian.topAppBar.TopAppBarDefaults
 import io.github.madmaximuus.persian.topAppBar.rememberTopAppBarState
 import ru.rabbit.persian.appShowcase.componets.SampleRow
@@ -50,6 +53,15 @@ object Charts : Screen {
             val (subtitle, onSubtitleChange) = remember { mutableStateOf(false) }
             val (label, onLabelChange) = remember { mutableStateOf(false) }
             val (total, onTotalChange) = remember { mutableStateOf(false) }
+            var selectedStyle by remember { mutableStateOf("Pie") }
+
+            val styleStates = remember {
+                listOf(
+                    mutableStateOf(true),
+                    mutableStateOf(false)
+                )
+            }
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -59,124 +71,154 @@ object Charts : Screen {
                     .navigationBarsPadding()
             ) {
                 SampleRow(text = "Sample", firstItem = true) {
-                    DonutChart(
-                        modifier = Modifier,
-                        size = 200.dp,
-                        data = listOf(
-                            ChartData(
-                                10.0,
-                                Color(0xFF5F00AA)
-                            ),
-                            ChartData(
-                                20.0,
-                                Color(0xFF004EC3)
-                            ),
-                            ChartData(
-                                30.0,
-                                Color(0xFF277A00)
-                            ),
-                            ChartData(
-                                40.0,
-                                Color(0xFFFFA800)
-                            ),
-                            ChartData(
-                                50.0,
-                                Color(0xFF9B0000)
-                            ),
-                        ),
-                        config = ChartConfig(
-                            showLabel = label,
-                            showTotalValueAsSubtitle = total,
-                            divided = divided
-                        ),
-                        title = if (title) "Title" else null,
-                        subtitle = if (subtitle) "Subtitle" else null,
-                        style = DonutChartDefaults.style()
-                    )
-                    PieChart(
-                        modifier = Modifier,
-                        size = 200.dp,
-                        data = listOf(
-                            ChartData(
-                                10.0,
-                                Color(0xFF5F00AA)
-                            ),
-                            ChartData(
-                                20.0,
-                                Color(0xFF004EC3)
-                            ),
-                            ChartData(
-                                30.0,
-                                Color(0xFF277A00)
-                            ),
-                            ChartData(
-                                40.0,
-                                Color(0xFFFFA800)
-                            ),
-                            ChartData(
-                                50.0,
-                                Color(0xFF9B0000)
-                            ),
-                        ),
-                        config = ChartConfig(
-                            showLabel = label,
-                            showTotalValueAsSubtitle = total,
-                            divided = divided
-                        ),
-                        style = PieChartDefaults.style()
-                    )
+                    when (selectedStyle) {
+                        "Pie" -> {
+                            PieChart(
+                                modifier = Modifier,
+                                data = listOf(
+                                    ChartData(
+                                        10.0,
+                                        Color(0xFF5F00AA)
+                                    ),
+                                    ChartData(
+                                        20.0,
+                                        Color(0xFF004EC3)
+                                    ),
+                                    ChartData(
+                                        30.0,
+                                        Color(0xFF277A00)
+                                    ),
+                                    ChartData(
+                                        40.0,
+                                        Color(0xFFFFA800)
+                                    ),
+                                    ChartData(
+                                        50.0,
+                                        Color(0xFF9B0000)
+                                    ),
+                                ),
+                                config = ChartConfig(
+                                    showLabel = label,
+                                    showTotalValueAsSubtitle = total,
+                                    divided = divided
+                                ),
+                                style = PieChartDefaults.style()
+                            )
+                        }
+
+                        "Donut" -> {
+                            DonutChart(
+                                modifier = Modifier,
+                                data = listOf(
+                                    ChartData(
+                                        10.0,
+                                        Color(0xFF5F00AA)
+                                    ),
+                                    ChartData(
+                                        20.0,
+                                        Color(0xFF004EC3)
+                                    ),
+                                    ChartData(
+                                        30.0,
+                                        Color(0xFF277A00)
+                                    ),
+                                    ChartData(
+                                        40.0,
+                                        Color(0xFFFFA800)
+                                    ),
+                                    ChartData(
+                                        50.0,
+                                        Color(0xFF9B0000)
+                                    ),
+                                ),
+                                config = ChartConfig(
+                                    showLabel = label,
+                                    showTotalValueAsSubtitle = total,
+                                    divided = divided,
+                                ),
+                                title = if (title) "Title" else null,
+                                subtitle = if (subtitle) "Subtitle" else null,
+                                style = DonutChartDefaults.style()
+                            )
+                        }
+                    }
                 }
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(
-                            top = PersianTheme.spacing.size8,
-                            bottom = PersianTheme.spacing.size8,
-                            start = PersianTheme.spacing.size12,
-                            end = PersianTheme.spacing.size12
-                        ),
+                        .padding(top = PersianTheme.spacing.size12),
                 ) {
-                    Text(
-                        text = "Settings",
-                        style = PersianTheme.typography.titleMedium,
-                        color = PersianTheme.colorScheme.onSurface
+                    Form(
+                        subhead = {
+                            Subhead(text = "Style")
+                        },
+                        content = {
+                            RadioButtons {
+                                RadioButton(
+                                    text = "Pie",
+                                    selected = styleStates[0].value,
+                                    onSelectedChange = {
+                                        styleStates.forEachIndexed { index, mutableState ->
+                                            mutableState.value = index == 0
+                                        }
+                                        selectedStyle = "Pie"
+                                    }
+                                )
+                                RadioButton(
+                                    text = "Donut",
+                                    selected = styleStates[1].value,
+                                    onSelectedChange = {
+                                        styleStates.forEachIndexed { index, mutableState ->
+                                            mutableState.value = index == 1
+                                        }
+                                        selectedStyle = "Donut"
+                                    }
+                                )
+                            }
+                        }
                     )
-                    Spacer(modifier = Modifier.height(PersianTheme.spacing.size4))
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Checkbox(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Show Total",
-                            checked = total,
-                            onCheckedChange = onTotalChange
-                        )
-                        Checkbox(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Show Label",
-                            checked = label,
-                            onCheckedChange = onLabelChange
-                        )
-                        Checkbox(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Title",
-                            checked = title,
-                            onCheckedChange = onTitleChange
-                        )
-                        Checkbox(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Subtitle",
-                            checked = subtitle,
-                            onCheckedChange = onSubtitleChange
-                        )
-                        Checkbox(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = "Divided",
-                            checked = divided,
-                            onCheckedChange = onDividedChange
-                        )
-                    }
+                    Form(
+                        modifier = Modifier.padding(top = PersianTheme.spacing.size12),
+                        subhead = {
+                            Subhead(text = "Settings")
+                        },
+                        content = {
+                            Checkboxes {
+                                Checkbox(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    text = "Labels",
+                                    checked = label,
+                                    onCheckedChange = onLabelChange
+                                )
+                                if (selectedStyle == "Donut") {
+                                    Checkbox(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = "Total",
+                                        checked = total,
+                                        onCheckedChange = onTotalChange
+                                    )
+                                    Checkbox(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = "Title",
+                                        checked = title,
+                                        onCheckedChange = onTitleChange
+                                    )
+                                    Checkbox(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = "Subtitle",
+                                        checked = subtitle,
+                                        onCheckedChange = onSubtitleChange
+                                    )
+                                    Checkbox(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = "Divided",
+                                        checked = divided,
+                                        onCheckedChange = onDividedChange
+                                    )
+                                }
+                            }
+                        }
+                    )
                 }
             }
         }
