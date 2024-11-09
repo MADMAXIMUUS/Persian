dd96f2db8356f5e807a784a53111b0cf371c698f
MADMAXIMUUS <maxim00nechaev@gmail.com>
2024-11-09 13:58:12 +0300

    Update: Skeleton

--- app-persian-showcase/src/main/java/ru/rabbit/persian/appShowcase/screens/Skeleton.kt
+++ app-persian-showcase/src/main/java/ru/rabbit/persian/appShowcase/screens/Skeleton.kt
@@ -1,13 +1,12 @@
 package ru.rabbit.persian.appShowcase.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,7 +23,6 @@ object Skeleton : Screen {
 
     override val navigation: String = "skeleton"
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content(navController: NavController?) {
         SampleScaffold(
@@ -55,15 +53,13 @@ object Skeleton : Screen {
                     )
                 }
                 items(4) {
-                    Row(
+                    Box(
                         modifier = Modifier
                             .fillMaxWidth()
                             .height(80.dp)
                             .padding(horizontal = PersianTheme.spacing.size12)
                             .shimmer(true)
-                    ) {
-
-                    }
+                    )
                 }
             }
         }
